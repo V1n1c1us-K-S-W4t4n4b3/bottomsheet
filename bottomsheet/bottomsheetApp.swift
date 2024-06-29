b339317b02eb15bf790550b5f60c95cbf5d8b86a
vinicius watanabe <vinikswatanabe@outlook.com>
2024-06-29 00:27:03 -0300

    Initial Commit

---
+++ bottomsheet/bottomsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  bottomsheetApp.swift
+//  bottomsheet
+//
+//  Created by Usuario on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct bottomsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
